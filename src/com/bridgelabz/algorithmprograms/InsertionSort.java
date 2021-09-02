0833925f7f318b00f84922ae22ae0744f4b9e837
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-03 05:13:45 +0530

    Refactored: Insertion Sort to use generics

--- src/com/bridgelabz/algorithmprograms/InsertionSort.java
+++ src/com/bridgelabz/algorithmprograms/InsertionSort.java
@@ -3,36 +3,62 @@ package com.bridgelabz.algorithmprograms;
 import java.util.Scanner;
 
 public class InsertionSort {
+	
+	public static <E extends Comparable<E>> void insertionSort(E listOfWords[]) {
 
-	public static String[] insertionSort(String listOfWords[]) {
+		for (int index = 0; index < listOfWords.length; index++) {
+			E wordToCheck = listOfWords[index];
+			int indexToCheck = index - 1;
 
-		for(int index=0;index<listOfWords.length;index++) {
-			String wordToCheck=listOfWords[index];
-			int indexToCheck=index-1;
-			while(indexToCheck>=0 && listOfWords[indexToCheck].compareTo(wordToCheck)>0) {
-				listOfWords[indexToCheck+1]=listOfWords[indexToCheck];
+			while (indexToCheck >= 0 && listOfWords[indexToCheck].compareTo(wordToCheck) > 0) {
+				listOfWords[indexToCheck + 1] = listOfWords[indexToCheck];
 				indexToCheck--;
 			}
-			listOfWords[indexToCheck+1]=wordToCheck;
+
+			listOfWords[indexToCheck + 1] = wordToCheck;
+
 		}
-		return listOfWords;
 	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter list of words:");
-		String string=scanner.nextLine();
+		System.out.print("Enter list of words to perform insertion sort: ");
+		String string = scanner.nextLine();
 		String listOfWords[] = string.toLowerCase().split(" ");
-		System.out.print("Before insertion sort: ");
-		for(int index=0;index<listOfWords.length;index++) {
-			System.out.print(listOfWords[index]+" ");
+		System.out.print("Before insertion sort , the words are: ");
+		for (int index = 0; index < listOfWords.length; index++) {
+			System.out.print(listOfWords[index] + " ");
 		}
 		System.out.println();
-		listOfWords=insertionSort(listOfWords);
-		System.out.print("After insertion sort: ");
-		for(int index=0;index<listOfWords.length;index++) {
-			System.out.print(listOfWords[index]+" ");
+		insertionSort(listOfWords);
+		System.out.print("After insertion sort , the words are: ");
+
+		for (int index = 0; index < listOfWords.length; index++) {
+			System.out.print(listOfWords[index] + " ");
+		}
+
+		System.out.print("\nEnter number of elements: ");
+		int size = scanner.nextInt();
+		System.out.print("Enter "+size+" numbers to perform insertion sort: ");
+
+		Integer arrayOfNumbers[] = new Integer[size];
+
+		for (int index = 0; index < size; index++) {
+			arrayOfNumbers[index] = scanner.nextInt();
+		}
+		System.out.print("Before insertion sort , the numbers are: ");
+
+		for (int index = 0; index < arrayOfNumbers.length; index++) {
+			System.out.print(arrayOfNumbers[index] + " ");
+		}
+		System.out.println();
+		insertionSort(arrayOfNumbers);
+		System.out.print("After insertion sort , the numbers are: ");
+
+		for (int index = 0; index < arrayOfNumbers.length; index++) {
+			System.out.print(arrayOfNumbers[index] + " ");
 		}
 		scanner.close();
+
 	}
 }
