2fa25419b1a68636f565c8d50d86a9f8c1d28ba9
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-03 05:05:32 +0530

    Refactored: Binary Search to use generics

--- src/com/bridgelabz/algorithmprograms/BinarySearch.java
+++ src/com/bridgelabz/algorithmprograms/BinarySearch.java
@@ -5,42 +5,44 @@ import java.util.Scanner;
 
 public class BinarySearch {
 
-	public static void binarySearch(String listOfWords[], int startIndex, int endIndex, String key) {
+	public static <E extends Comparable<E>> void binarySearch(E[] listOfWords,int firstIndex,int lastIndex,E key ) {
+		int middleIndex = (firstIndex + lastIndex) / 2;
 
-		int middleIndex = (startIndex + endIndex) / 2;
-		while (startIndex <= endIndex) {
+		while (firstIndex <= lastIndex) {
 			if (listOfWords[middleIndex].compareTo(key) < 0) {
-				startIndex = middleIndex + 1;
+				firstIndex = middleIndex + 1;
 			} else if (listOfWords[middleIndex].compareTo(key) == 0) {
 				System.out.println("Given word : " + key + " is found at index: " + middleIndex);
 				break;
 			} else {
-				endIndex = middleIndex - 1;
+				lastIndex = middleIndex - 1;
 			}
-			middleIndex = (startIndex + endIndex) / 2;
+			middleIndex = (firstIndex + lastIndex) / 2;
 		}
-		if (startIndex > endIndex) {
+		if (firstIndex > lastIndex) {
 			System.out.println("Given word : " + key + " is not found");
 		}
 
 	}
 
 	public static void main(String[] args) {
-		System.out.print("Enter the list of words :");
+		System.out.println("Enter the list of words :");
 		Scanner scanner = new Scanner(System.in);
 		String string = scanner.nextLine();
+
+		System.out.println("List of words after sorting:");
 		String listOfWords[] = string.toLowerCase().split(",");
+
 		Arrays.sort(listOfWords);
 
-		System.out.println("Sorted list of words:");
 		for (String word : listOfWords) {
 			System.out.print(word+" ");
 		}
-		System.out.print("\nEnter the key word to search: ");
-		String key = scanner.nextLine();
-		scanner.close();
+		System.out.println("\n\nEnter the key word to search");
+		String key = scanner.next();
 
+		Arrays.sort(listOfWords);
 		binarySearch(listOfWords, 0, (listOfWords.length) - 1, key);
-
+		scanner.close();
 	}
 }
